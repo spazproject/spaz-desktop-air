24205a8ccf835a7d3a80dc43c2e9e972cd5fe000
funkatron <funkatron@7bcef228-673c-0410-87d4-a540d69ff4b3>
2008-01-15 02:10:55 +0000

    emptying the trunk

--- themes/Dumb Terminal/info.js
+++
@@ -1,8 +0,0 @@
-var Spaz; if (!Spaz) Spaz = {};
-
-Spaz.Theme.info = {
-	name        : 'Dumb Terminal Theme',
-	author      : 'Ed Finkler',
-	description : 'Remeber when your monitor had one color?',
-	link        : 'http://funkatron.com/spaz'
-}
--- themes/whitespace/info.js
+++
@@ -1,8 +0,0 @@
-var Spaz; if (!Spaz) Spaz = {};
-
-Spaz.Theme.info = {
-	name        : 'Whitespace Theme',
-	author      : 'Ed Finkler',
-	description : 'Clean and light, with subtle shading. Or something',
-	link        : 'http://funkatron.com/spaz'
-}
