27859db8d9aa169e40bf47974891c3013446ed8c
funkatron <funkatron@7bcef228-673c-0410-87d4-a540d69ff4b3>
2008-09-08 19:59:03 +0000

    - Fixed introspector path lib in Spaz.Debug.insertDebugScripts

--- assets/spaz/spaz.debug.js
+++ assets/spaz/spaz.debug.js
@@ -108,8 +108,8 @@ Spaz.Debug.dumpHTMLSelectListener = function(event) {
 Spaz.Debug.insertDebugScripts = function() {
 	air.trace("INSERT DEBUGGING SCRIPTS");
 	var e = document.createElement("script");
-	e.src = "assets/AIRIntrospector.js";
-	e.type="text/javascript";
+	e.src = "assets/air/AIRIntrospector.js";
+	e.type= "text/javascript";
 	document.getElementsByTagName("head")[0].appendChild(e);
 	//<base href="app:/"/>
 	//<script src="assets/jquery/jquery-profile.js" type="text/javascript" charset="utf-8"></script>
