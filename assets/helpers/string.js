b8ea2f9c5cd695b9540438e1f8a24f0d0a7eb765
funkatron <funkatron@7bcef228-673c-0410-87d4-a540d69ff4b3>
2008-11-11 03:01:23 +0000

    added new string.js helper with escape_html function

---
+++ assets/helpers/string.js
@@ -0,0 +1,245 @@
+/*
+	escapes <,>,",',& into html entities
+*/
+function escape_html(string) {
+	return htmlspecialchars(string, 'ENT_QUOTES');
+}
+
+
+
+
+
+
+/* 
+ * More info at: http://kevin.vanzonneveld.net/techblog/category/php2js
+ * 
+ * php.js is copyright 2008 Kevin van Zonneveld.
+ * 
+ * Portions copyright Ates Goral (http://magnetiq.com), Legaev Andrey,
+ * _argos, Jonas Raoni Soares Silva (http://www.jsfromhell.com),
+ * Webtoolkit.info (http://www.webtoolkit.info/), Carlos R. L. Rodrigues, Ash
+ * Searle (http://hexmen.com/blog/), Tyler Akins (http://rumkin.com), mdsjack
+ * (http://www.mdsjack.bo.it), Alexander Ermolaev
+ * (http://snippets.dzone.com/user/AlexanderErmolaev), Andrea Giammarchi
+ * (http://webreflection.blogspot.com), Bayron Guevara, Cord, David, Karol
+ * Kowalski, Leslie Hoare, Lincoln Ramsay, Mick@el, Nick Callen, Peter-Paul
+ * Koch (http://www.quirksmode.org/js/beat.html), Philippe Baumann, Steve
+ * Clay, booeyOH
+ * 
+ * Licensed under the MIT (MIT-LICENSE.txt) license.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL KEVIN VAN ZONNEVELD BE LIABLE FOR ANY CLAIM, DAMAGES 
+ * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+function htmlspecialchars (string, quote_style) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Mirek Slugen
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   bugfixed by: Nathan
+    // +   bugfixed by: Arno
+    // +    revised by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // -    depends on: get_html_translation_table
+    // *     example 1: htmlspecialchars("<a href='test'>Test</a>", 'ENT_QUOTES');
+    // *     returns 1: '&lt;a href=&#039;test&#039;&gt;Test&lt;/a&gt;'
+
+    var histogram = {}, symbol = '', tmp_str = '', i = 0;
+    tmp_str = string.toString();
+
+    if (false === (histogram = get_html_translation_table('HTML_SPECIALCHARS', quote_style))) {
+        return false;
+    }
+
+	// first, do &amp;
+	tmp_str = tmp_str.split('&').join(histogram['&']);
+	
+	// then do the rest
+    for (symbol in histogram) {
+		if (symbol != '&') {
+			entity = histogram[symbol];
+	        tmp_str = tmp_str.split(symbol).join(entity);
+		}
+    }
+
+    return tmp_str;
+}
+
+
+
+function get_html_translation_table(table, quote_style) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Philip Peterson
+    // +    revised by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // %          note: It has been decided that we're not going to add global
+    // %          note: dependencies to php.js. Meaning the constants are not
+    // %          note: real constants, but strings instead. integers are also supported if someone
+    // %          note: chooses to create the constants themselves.
+    // %          note: Table from http://www.the-art-of-web.com/html/character-codes/
+    // *     example 1: get_html_translation_table('HTML_SPECIALCHARS');
+    // *     returns 1: {'"': '&quot;', '&': '&amp;', '<': '&lt;', '>': '&gt;'}
+    
+    var entities = {}, histogram = {}, decimal = 0, symbol = '';
+    var constMappingTable = {}, constMappingQuoteStyle = {};
+    var useTable = {}, useQuoteStyle = {};
+    
+    useTable      = (table ? table.toUpperCase() : 'HTML_SPECIALCHARS');
+    useQuoteStyle = (quote_style ? quote_style.toUpperCase() : 'ENT_COMPAT');
+    
+    // Translate arguments
+    constMappingTable[0]      = 'HTML_SPECIALCHARS';
+    constMappingTable[1]      = 'HTML_ENTITIES';
+    constMappingQuoteStyle[0] = 'ENT_NOQUOTES';
+    constMappingQuoteStyle[2] = 'ENT_COMPAT';
+    constMappingQuoteStyle[3] = 'ENT_QUOTES';
+    
+    // Map numbers to strings for compatibilty with PHP constants
+    if (!isNaN(useTable)) {
+        useTable = constMappingTable[useTable];
+    }
+    if (!isNaN(useQuoteStyle)) {
+        useQuoteStyle = constMappingQuoteStyle[useQuoteStyle];
+    }
+    
+    if (useTable == 'HTML_SPECIALCHARS') {
+        // ascii decimals for better compatibility
+        entities['60'] = '&lt;';
+        entities['62'] = '&gt;';
+        entities['38'] = '&amp;';
+    } else if (useTable == 'HTML_ENTITIES') {
+        // ascii decimals for better compatibility
+      entities['38'] = '&amp;';
+      entities['60'] = '&lt;';
+      entities['62'] = '&gt;';
+      entities['160'] = '&nbsp;';
+      entities['161'] = '&iexcl;';
+      entities['162'] = '&cent;';
+      entities['163'] = '&pound;';
+      entities['164'] = '&curren;';
+      entities['165'] = '&yen;';
+      entities['166'] = '&brvbar;';
+      entities['167'] = '&sect;';
+      entities['168'] = '&uml;';
+      entities['169'] = '&copy;';
+      entities['170'] = '&ordf;';
+      entities['171'] = '&laquo;';
+      entities['172'] = '&not;';
+      entities['173'] = '&shy;';
+      entities['174'] = '&reg;';
+      entities['175'] = '&macr;';
+      entities['176'] = '&deg;';
+      entities['177'] = '&plusmn;';
+      entities['178'] = '&sup2;';
+      entities['179'] = '&sup3;';
+      entities['180'] = '&acute;';
+      entities['181'] = '&micro;';
+      entities['182'] = '&para;';
+      entities['183'] = '&middot;';
+      entities['184'] = '&cedil;';
+      entities['185'] = '&sup1;';
+      entities['186'] = '&ordm;';
+      entities['187'] = '&raquo;';
+      entities['188'] = '&frac14;';
+      entities['189'] = '&frac12;';
+      entities['190'] = '&frac34;';
+      entities['191'] = '&iquest;';
+      entities['192'] = '&Agrave;';
+      entities['193'] = '&Aacute;';
+      entities['194'] = '&Acirc;';
+      entities['195'] = '&Atilde;';
+      entities['196'] = '&Auml;';
+      entities['197'] = '&Aring;';
+      entities['198'] = '&AElig;';
+      entities['199'] = '&Ccedil;';
+      entities['200'] = '&Egrave;';
+      entities['201'] = '&Eacute;';
+      entities['202'] = '&Ecirc;';
+      entities['203'] = '&Euml;';
+      entities['204'] = '&Igrave;';
+      entities['205'] = '&Iacute;';
+      entities['206'] = '&Icirc;';
+      entities['207'] = '&Iuml;';
+      entities['208'] = '&ETH;';
+      entities['209'] = '&Ntilde;';
+      entities['210'] = '&Ograve;';
+      entities['211'] = '&Oacute;';
+      entities['212'] = '&Ocirc;';
+      entities['213'] = '&Otilde;';
+      entities['214'] = '&Ouml;';
+      entities['215'] = '&times;';
+      entities['216'] = '&Oslash;';
+      entities['217'] = '&Ugrave;';
+      entities['218'] = '&Uacute;';
+      entities['219'] = '&Ucirc;';
+      entities['220'] = '&Uuml;';
+      entities['221'] = '&Yacute;';
+      entities['222'] = '&THORN;';
+      entities['223'] = '&szlig;';
+      entities['224'] = '&agrave;';
+      entities['225'] = '&aacute;';
+      entities['226'] = '&acirc;';
+      entities['227'] = '&atilde;';
+      entities['228'] = '&auml;';
+      entities['229'] = '&aring;';
+      entities['230'] = '&aelig;';
+      entities['231'] = '&ccedil;';
+      entities['232'] = '&egrave;';
+      entities['233'] = '&eacute;';
+      entities['234'] = '&ecirc;';
+      entities['235'] = '&euml;';
+      entities['236'] = '&igrave;';
+      entities['237'] = '&iacute;';
+      entities['238'] = '&icirc;';
+      entities['239'] = '&iuml;';
+      entities['240'] = '&eth;';
+      entities['241'] = '&ntilde;';
+      entities['242'] = '&ograve;';
+      entities['243'] = '&oacute;';
+      entities['244'] = '&ocirc;';
+      entities['245'] = '&otilde;';
+      entities['246'] = '&ouml;';
+      entities['247'] = '&divide;';
+      entities['248'] = '&oslash;';
+      entities['249'] = '&ugrave;';
+      entities['250'] = '&uacute;';
+      entities['251'] = '&ucirc;';
+      entities['252'] = '&uuml;';
+      entities['253'] = '&yacute;';
+      entities['254'] = '&thorn;';
+      entities['255'] = '&yuml;';
+    } else {
+        throw Error("Table: "+useTable+' not supported');
+        return false;
+    }
+    
+    if (useQuoteStyle != 'ENT_NOQUOTES') {
+        entities['34'] = '&quot;';
+    }
+    
+    if (useQuoteStyle == 'ENT_QUOTES') {
+        entities['39'] = '&#039;';
+    }
+    
+    // ascii decimals to real symbols
+    for (decimal in entities) {
+        symbol = String.fromCharCode(decimal)
+        histogram[symbol] = entities[decimal];
+    }
+    
+    return histogram;
+}
