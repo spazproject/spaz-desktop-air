dc69fe2bc0e42fef626bbf7201d3fc51ca5d4c0a
ronald.devera <ronald.devera@7bcef228-673c-0410-87d4-a540d69ff4b3>
2009-05-09 14:03:24 +0000

    Attempting to re-add Leopaz to 0.8 branch

---
+++ themes/Leopaz/info.js
@@ -0,0 +1,9 @@
+var Spaz; if (!Spaz) Spaz = {};
+
+Spaz.Theme.info = {
+	name        : 'Leopaz',
+	author      : 'Ron DeVera',
+	description : 'Spaz theme based on Mac OS X 10.5 (Leopard)',
+	link        : 'http://rondevera.com',
+	version     : 'v0.3'
+}
