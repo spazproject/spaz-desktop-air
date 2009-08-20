9619fe6d99ab53f20bd52f4fe018f38229fe7d4b
funkatron <funkatron@7bcef228-673c-0410-87d4-a540d69ff4b3>
2009-08-20 21:09:50 +0000

    more work on JR models

--- assets/models/Tweets.js
+++ assets/models/Tweets.js
@@ -1,33 +1,47 @@
-var Tweet = new JazzRecord.Model({
-  table: "tweets",
-  belongsTo: {user: "users"},
-  columns: {
-    user_id: "integer",
-    favorited: "bool",
-    in_reply_to_user_id: "integer",
-    in_reply_to_screen_name: "string",
-    text: "text",
-    created_at: "string",
-    truncated: "bool",
-    twitter_id: "integer",
-    in_reply_to_status_id: "integer",
-    source: "string",
-    SC_timeline_from: "string",
-    SC_user_received_by: "string",
-    SC_is_reply: "bool",
-    SC_created_at_unixtime: "integer",
-    SC_text_raw: "text",
-    SC_retrieved_unixtime: "integer",
-    isSent: "bool"
-  },
-  recordMethods: {
-	saveTweet: function(obj) {
-		
+var TweetModel = new JazzRecord.Model({
+	table: "tweets",
+	belongsTo: {
+		user: "twusers"
 	},
-    send: function() {
-      //code to send tweet
-    }
-  }
+	columns: {
+		user_id: "integer",
+		favorited: "bool",
+		in_reply_to_user_id: "integer",
+		in_reply_to_screen_name: "string",
+		text: "text",
+		created_at: "string",
+		truncated: "bool",
+		twitter_id: "integer",
+		in_reply_to_status_id: "integer",
+		source: "string",
+		SC_timeline_from: "string",
+		SC_user_received_by: "string",
+		SC_is_reply: "bool",
+		SC_created_at_unixtime: "integer",
+		SC_text_raw: "text",
+		SC_retrieved_unixtime: "integer",
+		isSent: "bool"
+	},
+	recordMethods: {
+		send: function() {
+			//code to send tweet
+			}
+	},
+	modelMethods: {
+		saveTweet : function(obj) {
+			var obj.twitter_id = obj.id;
+			delete obj.id;
+
+			obj.user.twitter_id = obj.user.id;
+			delete obj.user.id;
+
+			var user_id = TwUserModel.findOrCreate(obj.user);
+			delete obj.user;
+			
+			obj.user_id = user_id;
+			return this.create(obj);
+		}
+	}
 });
 
 
--- assets/models/Users.js
+++ assets/models/Users.js
@@ -1,56 +1,107 @@
-var User = new JazzRecord.Model({
-  table: "users",
-  foreignKey: "user_id",
-  hasMany: {tweets: "tweets"},
-  columns: {
-    profile_sidebar_border_color:"string",
-    description:"text",
-    utc_offset: "integer",
-    following: "bool",
-    profile_text_color: "string",
-    profile_background_image_url: "string",
-    followers_count: "integer",
-    url: "string",
-    profile_image_url:"string",
-    name:"string",
-    statuses_count: "integer",
-    profile_link_color:"string",
-    protected: "bool",
-    notifications: "bool",
-    profile_background_tile: "bool",
-    created_at: "string",
-    friends_count: "integer",
-    screen_name: "string",
-    profile_background_color: "string",
-    favourites_count: "integer",
-    verified: "bool",
-    profile_sidebar_fill_color: "string",
-    location: "string",
-    twitter_id: "integer",
-    time_zone:"string"
-  },
-  recordMethods: {
-	userExists: function(screen_name) {
-		var count = this.count('screen_name LIKE '+'\''+screen_name+'\'');
-		if (count > 0) {
-			return true;
-		} else {
-			return false;
+var TwUserModel = new JazzRecord.Model({
+	table: "twusers",
+	foreignKey: "user_id",
+	hasMany: {
+		tweets: "tweets"
+	},
+	columns: {
+		profile_sidebar_border_color: "string",
+		description: "text",
+		utc_offset: "integer",
+		following: "bool",
+		profile_text_color: "string",
+		profile_background_image_url: "string",
+		followers_count: "integer",
+		url: "string",
+		profile_image_url: "string",
+		name: "string",
+		statuses_count: "integer",
+		profile_link_color: "string",
+		protected: "bool",
+		notifications: "bool",
+		profile_background_tile: "bool",
+		created_at: "string",
+		friends_count: "integer",
+		screen_name: "string",
+		profile_background_color: "string",
+		favourites_count: "integer",
+		verified: "bool",
+		profile_sidebar_fill_color: "string",
+		location: "string",
+		twitter_id: "integer",
+		time_zone: "string"
+	},
+	recordMethods: {
+		newTweet: function() {
+			return Tweet.newRecord({
+				user_id: this.id
+			});
 		}
 	},
-	getUserById: function(twitter_id) {
-		return this.findBy('twitter_id', twitter_id);
-	},
-	getUser: function(screen_name) {
-		return this.findBy('screen_name', screen_name);
-	},
-	getTweetsByUser: function(screen_name) {
-		var user = this.getUser(screen_name);
-		return user.load('Tweets', 1);
-	},
-    newTweet: function() {
-	
-      return Tweet.newRecord({user_id: this.id});
-    }
-  }
+
+	modelMethods: {
+		userExists: function(screen_name, return_id) {
+			if (return_id) {
+				var user = this.findBy('screen_name', screen_name);
+				if (user) {
+					return user.id;
+				} else {
+					return false;
+				}
+			} else {
+				var count = this.count('screen_name LIKE ' + '\'' + screen_name + '\'');
+				if (count > 0) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+		},
+
+		userExistsId: function(twitter_id, return_id) {
+			if (return_id) {
+				var user = this.findBy('twitter_id', twitter_id);
+				if (user) {
+					return user.id;
+				} else {
+					return false;
+				}
+			} else {
+				var count = this.count('twitter_id = ' + twitter_id);
+				if (count > 0) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+		},
+
+		/**
+		 * given a user object, return the id if it exists, or create a new record
+		 * and return that id 
+		 */
+		findOrCreate: function(userobj) {
+			var user_id;
+			if ((user_id = this.userExistsId(userobj.id, true))) {
+				return user_id;
+			} else {
+				userobj.twitter_id = userobj.id;
+				delete userobj.id;
+				return this.created(userobj).id;
+			}
+		},
+
+		getUserById: function(twitter_id) {
+			return this.findBy('twitter_id', twitter_id);
+		},
+
+		getUser: function(screen_name) {
+			return this.findBy('screen_name', screen_name);
+		},
+
+		getTweetsByUser: function(screen_name) {
+			var user = this.getUser(screen_name);
+			return user.load('Tweets', 1);
+		},
+	}
 });
