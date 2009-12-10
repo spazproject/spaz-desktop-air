edc3b64011f4133f7b77ad502c6f5b968b62fb1b
ronald.devera <ronald.devera@7bcef228-673c-0410-87d4-a540d69ff4b3>
2009-12-10 02:57:42 +0000

    - Changed timeline avatars from <img>s to <div>s w/bgs; allows for rounded corners, as in Leopaz

--- app/views/spaz.tpl.js
+++ app/views/spaz.tpl.js
@@ -33,7 +33,7 @@ Spaz.Templates.timeline_entry = function(d) {
 	}
 	entryHTML += '"  data-status-id="'+d.id+'" data-user-screen_name="'+d.user.screen_name+'" data-user-id="'+d.user.id+'" data-timestamp="'+d.SC_created_at_unixtime+'">';
 	entryHTML += '	<div class="user" id="user-'+d.user.id+'" user-screen_name="'+d.user.screen_name+'">';
-	entryHTML += '		<img class="user-image clickable" height="48" width="48" src="'+d.user.profile_image_url+'" alt="'+d.user.screen_name+'" title="View user\'s profile" user-id="'+d.user.id+'" user-screen_name="'+d.user.screen_name+'" />';
+  entryHTML += '    <div class="user-image clickable" style="background-image:url('+d.user.profile_image_url+')" title="View user\'s profile" user-id="'+d.user.id+'" user-screen_name="'+d.user.screen_name+'">'+d.user.screen_name+'</div>';
 	entryHTML += '		<div class="user-screen-name clickable" title="View user\'s profile" user-id="'+d.user.id+'" user-screen_name="'+d.user.screen_name+'">'+d.user.screen_name+'</div>';
 	entryHTML += '	</div>';
 	entryHTML += '	<div class="status" id="status-'+d.id+'">';
