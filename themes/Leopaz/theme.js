948e248ec78d0f38a07930ee6542b963cc95efb5
Ron DeVera <ronald.devera@gmail.com>
2009-12-21 21:20:20 -0500

    Leopaz: Added JS snippets to theme.js for customizing scrollbars

--- themes/Leopaz/theme.js
+++ themes/Leopaz/theme.js
@@ -1,1 +1,11 @@
-// Not used yet
+// You can add the following to your user.js file to customize the UI:
+
+
+
+/*** Scrollbars ***/
+
+// No arrows at the top, and doubled arrows at the bottom
+// $('body').addClass('scrollbar-double-end');
+
+// Doubled arrows at both the top and the bottom
+// $('body').addClass('scrollbar-double-both');
