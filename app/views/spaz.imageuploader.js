e38b7565b36c9dbd5208e649c7f822a177172077
Ed Finkler <coj@funkatron.com>
2010-08-22 22:10:00 -0400

    Converted SpazImageUploader to work with new setup and SpazCore lib

--- app/views/spaz.imageuploader.js
+++ app/views/spaz.imageuploader.js
@@ -1,99 +1,69 @@
 Spaz.ImageUploader = function() {
 	
-	var thisA = this;
+	var that = this;
 	
-	this.SFU = new SpazFileUploader();
+	this.SIU = new SpazImageUploader();
 	
-	this.service_url = null;
-	this.service = null;
-	
-
-
-
 	this.init = function() {
 		
-		var container = $('#imageUploadWindow');
+		var container = jQuery('#imageUploadWindow');
 		
-		$('#imageupload-loading').hide();
+		jQuery('#imageupload-loading').hide();
 		
-		var target = $('.content', container).get(0);
-		target.addEventListener("dragenter", thisA.dragEnterOverHandler);
-		target.addEventListener("dragover", thisA.dragEnterOverHandler);
-		target.addEventListener("drop", thisA.dropHandler);
+		var target = jQuery('.content', container).get(0);
+		target.addEventListener("dragenter", that.dragEnterOverHandler);
+		target.addEventListener("dragover", that.dragEnterOverHandler);
+		target.addEventListener("drop", that.dropHandler);
 
 		// get the pref
-		thisA.service = Spaz.Prefs.get('file-uploader');
-		thisA.service_url = thisA.SFU.apis[thisA.service].upload_url;
-		sch.debug("service is "+ thisA.service);
-		sch.debug("service url "+ thisA.service_url);
-
 		var sharepass = Spaz.Prefs.get('services-twitpic-sharepassword');
 		sch.debug("sharepass is "+ sharepass);
 		
 		/*
 			populate the dropdown
 		*/
-		var labels = this.SFU.getAPILabels();
+		var labels = this.SIU.getServiceLabels();
 
 		for (var i=0; i < labels.length; i++) {
 			var this_service = labels[i];
-			if (this_service == thisA.service) {
-				$('#imageupload-file-uploader').append('<option value="'+this_service+'" selected="selected">'+this_service+'</option>');
-				$('#imageupload-extra-'+thisA.service).show();
-			} else {
-				$('#imageupload-file-uploader').append('<option value="'+this_service+'">'+this_service+'</option>');
-				$('#imageupload-extra-'+thisA.service).hide();
-			}			
+			jQuery('#imageupload-file-uploader').append('<option value="'+this_service+'">'+this_service+'</option>');
 		};
+		// set dropdown value
+		jQuery('#imageupload-file-uploader').val(Spaz.Prefs.get('file-uploader'));
+		
 		
 		/*
 			bind click on droplet
 		*/
-		$('#imageupload-droplet').click(function() {
-			thisA.browseForImage();
+		jQuery('#imageupload-droplet').click(function() {
+			that.browseForImage();
 		});
 		
 		
 		/*
 			bind service dropdown
 		*/
-		$('#imageupload-file-uploader').bind('change', function() {
-		    alert('changed');
-			thisA.service = $('#imageupload-file-uploader').val();
-			Spaz.Prefs.set('file-uploader', thisA.service);
-			
-			thisA.service_url = thisA.SFU.apis[thisA.service].upload_url;
-			sch.debug("service is "+ thisA.service);
-			sch.debug("service url "+ thisA.service_url);
-			
-			$('.service-extras', thisA.container).fadeOut();
-			$('#imageupload-extra-'+thisA.service).fadeIn();
-			
+		jQuery('#imageupload-file-uploader').bind('change', function() {
+			Spaz.Prefs.set('file-uploader', jQuery('#imageupload-file-uploader').val());
 		});
 		
 
-		
-		
-		// bind click to shorten action
-		$('#imageupload-button').bind('click', function() {
-			var fileUrl = $('#imageupload-file-url').val();
+		// bind click to upload
+		jQuery('#imageupload-button').bind('click', function() {
+			var fileUrl = jQuery('#imageupload-file-url').val();
 			if (fileUrl) {
-				thisA.uploadDraggedImage(fileUrl);
+				that.uploadDraggedImage(fileUrl);
 			} else {
-				thisA.browseForImage();
+				that.browseForImage();
 			}
-			
-			// var service = Spaz.Prefs.get('file-uploader');
-			// sch.debug("service is "+ service);
-			// Spaz.Upload[service]($('#shorten-original-link').val());
 		});
 		
 		
-		$('#imageupload-post-message').keyup(function(e) {
-			if ($('#imageupload-post-message').val().length>110) {
-				$('#imageupload-post-message').val( $('#imageupload-post-message').val().substr(0,110) );
+		jQuery('#imageupload-post-message').keyup(function(e) {
+			if (jQuery('#imageupload-post-message').val().length>110) {
+				jQuery('#imageupload-post-message').val( jQuery('#imageupload-post-message').val().substr(0,110) );
 			}
-			sch.debug($('#imageupload-post-message').val().length);
+			sch.debug(jQuery('#imageupload-post-message').val().length);
 		});
 				
 		// sch.debug(air.NativeApplication.nativeApplication.spazPrefs);
@@ -101,7 +71,9 @@ Spaz.ImageUploader = function() {
 
 
 
-
+	/*
+		Browse for the image!
+	*/
 	this.browseForImage = function() {
 		var imageFilter = new air.FileFilter("Images", "*.jpg;*.jpeg;*.gif;*.png");
 		var userFile = new air.File();
@@ -121,7 +93,7 @@ Spaz.ImageUploader = function() {
 				alert("File must be one of the following:\n .jpg, .jpeg, .gif, .png");
 				return false;
 			}
-			thisA.handleDrop(event.target.url);
+			that.displayChosenFile(event.target.url);
 			return true;
 		});
 	};
@@ -132,28 +104,31 @@ Spaz.ImageUploader = function() {
 	*/
 	this.prepPhotoPost = function(url) {
 	    if (url) {
-	        var msg = $('#imageupload-post-message').val() + ' ' + url;
-	
-			Spaz.postPanel.prepPhotoPost(msg);
-			
+			Spaz.postPanel.prepPhotoPost(url);			
 	        return true;
 	    } else {
 	        return false;
 	    }
 	};
 
-	this.handleDrop = function(fileUrl) {
-		$('#imageupload-droplet').html('<img src="'+fileUrl+'" />');
-		$('#imageupload-droplet>img').css('width', $('#imageupload-droplet').width());
-		$('#imageupload-droplet>img').css('height', $('#imageupload-droplet').height());
-		$('#imageupload-file-url').val(fileUrl);
+	/*
+		handle dropped file
+	*/
+	this.displayChosenFile = function(fileUrl) {
+		jQuery('#imageupload-droplet').html('<img src="'+fileUrl+'" />');
+		jQuery('#imageupload-droplet>img').css('width', jQuery('#imageupload-droplet').width());
+		jQuery('#imageupload-droplet>img').css('height', jQuery('#imageupload-droplet').height());
+		jQuery('#imageupload-file-url').val(fileUrl);
 	};
 
-	
+	/*
+		prevent default happening
+	*/
 	this.dragEnterOverHandler = function(event){
 	    event.preventDefault();
 	};
 
+	
 	this.dropHandler = function(event){
 
 		event.preventDefault();
@@ -172,7 +147,7 @@ Spaz.ImageUploader = function() {
 			alert("File must be one of the following:\n .jpg, .jpeg, .gif, .png");
 			return false;
 		}
-		thisA.handleDrop(fileUrl);
+		that.displayChosenFile(fileUrl);
 		return true;
 	};
 
@@ -180,55 +155,52 @@ Spaz.ImageUploader = function() {
 		Upload the dragged image to the service
 	*/
 	this.uploadDraggedImage = function(fileUrl) {
-		sch.debug(fileUrl);
-
-		Spaz.UI.showLoading();
-
-		alert('This is NOT gonna work until rewritten for oAuth');
-
-		// upload the file
-		Spaz.Data.uploadFile({
-			'extra'  :{
-				"username": Spaz.Prefs.getUsername(),
-				"password": Spaz.Prefs.getPassword(),
-				"source"  : Spaz.Prefs.get('twitter-source'),
-				"message" : $('#imageupload-post-message').val()
-			},
-			'url'    :thisA.service_url,
-			'fileUrl':fileUrl,
-			'open'   : Spaz.UI.showLoading,
-			'complete': function(event) {
-				Spaz.UI.hideLoading();
-
-				var loader = event.target;
-			    sch.debug(loader.data);
-
-				var parser=new DOMParser();
-				xmldoc = parser.parseFromString(loader.data,"text/xml");
-
-				//var mediaurl = $(xmldoc).find('mediaurl').text();
-				//sch.debug(mediaurl);
-				//prepPhotoPost(mediaurl);
-				//$('#status-text').html('Complete');
-				var rspAttr = xmldoc.getElementsByTagName("rsp")[0].attributes;
-				if (rspAttr.getNamedItem("stat").nodeValue == 'ok')
-				{
-					var mediaurl = $(xmldoc).find('mediaurl').text();
-					sch.debug(mediaurl);
-					thisA.prepPhotoPost(mediaurl);
-					$('#imageupload-status-text').html('Complete');
+		var service = Spaz.Prefs.get('file-uploader');
+		
+		if (this.SIU.getServiceLabels().indexOf(service) === -1 ) {
+			Spaz.Prefs.set('file-uploader', this.SIU.getServiceLabels()[0]);
+		}
+		
+		var auth = Spaz.Prefs.getAuthObject();
+		var image_uploader = new SpazImageUploader();
+		
+		jQuery('#imageupload-loading').show();
+		jQuery('#imageupload-status-text').show().text('Uploading…');
+		
+		image_uploader_opts = {
+			'auth_obj': auth,
+			'service' : Spaz.Prefs.get('file-uploader') || 'drippic',
+			'file_url': fileUrl,
+			'onSuccess':function(event_data) { // onSuccess
+				jQuery('#imageupload-loading').hide();
+				if (event_data.url) {
+					that.prepPhotoPost(event_data.url);
+					jQuery('#imageupload-status-text').html($L('Complete'));
 					Spaz.UI.closePopbox();
-				} 
-				else
-				{
-					var errAttributes = xmldoc.getElementsByTagName("err")[0].attributes;
-					sch.debug(errAttributes);
-					errMsg = errAttributes.getNamedItem("msg").nodeValue;
-					sch.debug(errMsg);
-					$('#imageupload-status-text').html(errMsg);
-				} 
+				} else if (event_data.error) {
+					jQuery('#imageupload-status-text').html($L("Posting image failed:") + " " + event_data.error);
+				} else {
+					jQuery('#imageupload-status-text').html($L("Posting image failed"));
+				}
+			},
+			'onFailure':function(event_obj) { // onFailure
+				sch.error('Posting image FAILED');
+				ech.error("Error!");
+				ech.error(event_obj);
+				jQuery('#imageupload-loading').hide();
+				jQuery('#imageupload-status-text').html($L("Posting image failed"));
 			}
-		});
+		};
+		
+		// force pikchur uploading if using identi.ca
+		if (Spaz.Prefs.getAccountType() == SPAZCORE_ACCOUNT_IDENTICA) {
+			image_uploader_opts['service'] = 'pikchur';
+			image_uploader_opts['extra']['service'] = 'identi.ca';
+		}
+		
+		image_uploader.setOpts(image_uploader_opts);
+		image_uploader.upload();
+		
 	};
 };
 
