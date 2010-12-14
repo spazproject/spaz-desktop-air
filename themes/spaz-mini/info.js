09630e6547428e484542e948821e57db90f42f66
Ed Finkler <coj@funkatron.com>
2010-12-14 15:58:49 -0500

    deleted a bunch of files

--- themes/spaz-mini/info.js
+++
@@ -1,6 +0,0 @@
-{
-	"name"        : "Spaz Mini Theme",
-	"author"      : "Ed Finkler",
-	"description" : "A mini version of the default theme. Emphasis on small-osity",
-	"link"        : "http://getspaz.com"
-}
